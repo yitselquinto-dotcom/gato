ae72157f11b5ecaa7317ab16962d36670b534cbf
yitselquinto-dotcom <yitselquinto@gmail.com>
2025-08-21 21:11:34 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+function Call () {
+	
+}
+let Brigthtness = 63
+let StartHours = 19
+let mealsPrDay = 9
+mealsPrDay = 9
+let gram_pr_second = 0.93
+let gramPrDay = 50
+basic.forever(function () {
+	
+})
